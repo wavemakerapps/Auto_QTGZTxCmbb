3adfda50c380c3bce92d06cf017c93fb896e4f3c
root <root@d3a2e0704417>
2017-06-03 17:45:35 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_QTGZTxCmbb",
+  "homePage" : "Main",
+  "name" : "Auto_QTGZTxCmbb",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
